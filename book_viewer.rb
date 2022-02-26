527d9485b465e8785f7ade89d7800870c0508754
Joe Webster <joseph.scott.webster@gmail.com>
2022-02-26 12:51:21 -0500

    Add a chapter page

--- book_viewer.rb
+++ book_viewer.rb
@@ -6,8 +6,14 @@ require 'tilt/erubis'
 
 get '/' do
   @title = 'The Adventures of Sherlock Holmes!'
-
   @contents = File.readlines 'data/toc.txt'
-  # @chapters = contents.split("\n")
   erb :home
+end
+
+get '/chapters/1' do
+  @title = 'Chapter 1'
+  @contents = File.readlines 'data/toc.txt'
+  @paragraphs = File.read('data/chp1.txt').split("\n\n")
+
+  erb :chapter
 end
