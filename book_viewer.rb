4fb3a9eaedb37dd5d5218b4f3b8cee56e17a80e1
Joe Webster <joseph.scott.webster@gmail.com>
2022-03-12 12:48:56 -0500

    Search results link to specific chapter paragraphs

--- book_viewer.rb
+++ book_viewer.rb
@@ -28,6 +28,7 @@ end
 get '/search' do
   @query = params[:query]
   @results = matching_chapters(@query)
+  # require 'pry'; binding.pry
   erb :search
 end
 
@@ -37,26 +38,32 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map { |para| "<p>#{para}</p>" }.join
+    text.split("\n\n").map.with_index do |para, index|
+      "<p id='paragraph#{index}'>#{para}</p>"
+    end.join
   end
+end
 
-  def each_chapter
-    @contents.each_with_index do |name, index|
-      chap_num = index + 1
-      chapter = File.read("data/chp#{chap_num}.txt")
-      yield name, chap_num, chapter
+def each_chapter
+  @contents.each_with_index do |name, index|
+    chap_num = index + 1
+    chapter = File.read("data/chp#{chap_num}.txt")
+    yield name, chap_num, chapter
+  end
+end
+
+def matching_chapters(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |name, number, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
     end
+    results << { number: number, name: name, paragraphs: matches } if matches.any?
   end
 
-  def matching_chapters(query)
-    results = []
-
-    return results if !query || query.empty?
-
-    each_chapter do |name, number, contents|
-      results << { number: number, name: name } if contents.include?(query)
-    end
-
-    results
-  end
+  results
 end
